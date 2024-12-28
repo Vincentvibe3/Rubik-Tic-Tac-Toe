5552950ab217bc4a092658bcfcc2639b1500c0fd
Vincent <vincentvibe3@gmail.com>
2024-12-27 20:03:11 -0500

    Embed version number

---
+++ scripts/GetVersion.gd
@@ -0,0 +1,6 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = ProjectSettings.get_setting_with_override("application/config/version")
